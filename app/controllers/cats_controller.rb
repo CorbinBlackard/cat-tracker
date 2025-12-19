7ec815a7924b40c2abf740e48e8566dfeb91139e
CorbinBlackard <59624967+CorbinBlackard@users.noreply.github.com>
2025-12-19 14:37:08 -0500

    fixed edit/create bugs

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,99 +1,102 @@
 class CatsController < ApplicationController
-   before_action :set_cat, only: %i[ show edit update destroy ]
+  before_action :set_cat, only: %i[ show edit update destroy ]
 
-   # GET /cats or /cats.json
-   def index
-      @cats = Cat.all
-   end
+  # GET /cats or /cats.json
+  def index
+    @cats = Cat.all
+  end
 
-   # GET /cats/1 or /cats/1.json
-   def show
-      @cats = Cat.find(params[:id])
-   end
+  # GET /cats/1 or /cats/1.json
+  def show
+    # Remove this line - it's redundant with before_action
+    # @cats = Cat.find(params[:id])
+  end
 
-   # GET /cats/new
-   def new
-      @cat = Cat.new
-   end
+  # GET /cats/new
+  def new
+    @cat = Cat.new
+  end
 
-   # GET /cats/1/edit
-   def edit
-   end
+  # GET /cats/1/edit
+  def edit
+  end
 
-   def feed
-      @cat = Cat.find(params[:id])
-      if @cat.feed!
-         redirect_to @cat, notice: "#{@cat.name} has been fed! 🐟"
+  def feed
+    @cat = Cat.find(params[:id])
+    if @cat.feed!
+      redirect_to @cat, notice: "#{@cat.name} has been fed! 🐟"
+    else
+      redirect_to @cat, alert: "Could not feed #{@cat.name}: #{@cat.errors.full_messages.join(', ')}"
+    end
+  end
+
+  # POST /cats or /cats.json
+  def create
+    @cat = Cat.new(cat_params)
+
+    respond_to do |format|
+      if @cat.save
+        format.html { redirect_to @cat, notice: "Cat was successfully created." }
+        format.json { render :show, status: :created, location: @cat }
       else
-         redirect_to @cat, alert: "Could not feed #{@cat.name}: #{@cat.errors.full_messages.join(', ')}"
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @cat.errors, status: :unprocessable_entity }
       end
-   end
+    end
+  end
 
-   # POST /cats or /cats.json
-   def create
-      @cat = Cat.new(cat_params)
-
-      respond_to do |format|
-         if @cat.save
-         format.html { redirect_to @cat, notice: "Cat was successfully created." }
-         format.json { render :show, status: :created, location: @cat }
-         else
-         format.html { render :new, status: :unprocessable_entity }
-         format.json { render json: @cat.errors, status: :unprocessable_entity }
-         end
+  # PATCH/PUT /cats/1 or /cats/1.json
+  def update
+    respond_to do |format|
+      if @cat.update(cat_params)
+        format.html { redirect_to @cat, notice: "Cat was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @cat }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @cat.errors, status: :unprocessable_entity }
       end
-   end
+    end
+  end
 
-   # PATCH/PUT /cats/1 or /cats/1.json
-   def update
-      respond_to do |format|
-         if @cat.update(cat_params)
-         format.html { redirect_to @cat, notice: "Cat was successfully updated.", status: :see_other }
-         format.json { render :show, status: :ok, location: @cat }
-         else
-         format.html { render :edit, status: :unprocessable_entity }
-         format.json { render json: @cat.errors, status: :unprocessable_entity }
-         end
-      end
-   end
+  # DELETE /cats/1 or /cats/1.json
+  def destroy
+    @cat.destroy!
 
-   # DELETE /cats/1 or /cats/1.json
-   def destroy
-      @cat.destroy!
+    respond_to do |format|
+      format.html { redirect_to cats_path, notice: "Cat was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
 
-      respond_to do |format|
-         format.html { redirect_to cats_path, notice: "Cat was successfully destroyed.", status: :see_other }
-         format.json { head :no_content }
-      end
-   end
+  def spotted
+    @cat = Cat.find(params[:id])
+    @cat.spotted!
+    redirect_to root_path, notice: "You spotted #{@cat.name}!"
+  end
 
-   def spotted
-      @cat = Cat.find(params[:id])
-      @cat.spotted!
-      redirect_to root_path, notice: "You spotted #{@cat.name}!"
-   end
+  def reset_spots
+    @cat = Cat.find(params[:id])
+    @cat.reset_spots
+    redirect_to root_path, notice: "#{@cat.name} spottings had been reset"
+  end
 
-   def reset_spots
-      @cat = Cat.find(params[:id])
-      @cat.reset_spots
-      redirect_to root_path, notice: "#{@cat.name} spottings had been reset"
-   end
+  def reset_feeding
+    @cat = Cat.find(params[:id])
+    @cat.update(times_fed_today: 0, last_fed_at: nil)
 
-   def reset_feeding
-      @cat = Cat.find(params[:id])
-      @cat.update(times_fed_today: 0, last_fed_at: nil)
+    redirect_to @cat, notice: "#{@cat.name}'s feeding data has been reset!"
+  end
 
-      redirect_to @cat, notice: "#{@cat.name}'s feeding data has been reset!"
-   end
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cat
+    @cat = Cat.find(params[:id])  # FIXED: Changed .expect to [:id]
+  end
 
-   private
-   # Use callbacks to share common setup or constraints between actions.
-   def set_cat
-      @cat = Cat.find(params.expect(:id))
-   end
-
-   # Only allow a list of trusted parameters through.
-   def cat_params
-      params.expect(cat: [ :name, :color, :pattern, :temperament, :location, :last_seen, :notes, :favorite_food, :times_spotted ])
-   end
+  # Only allow a list of trusted parameters through.
+  def cat_params
+    params.require(:cat).permit(:name, :color, :pattern, :temperament, :location,
+                                :last_seen, :notes, :favorite_food, :times_spotted)
+    # FIXED: Changed .expect to .require and fixed the parameter structure
+  end
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,19 +1,19 @@
 class Cat < ApplicationRecord
-   # Required fields with better error messages
-   validates :name,
+  # Required fields with better error messages
+  validates :name,
       presence: { message: "Every cat needs a name!" },
       length: { minimum: 2, too_short: "Name must be at least 2 characters" }
 
-   validates :color,
+  validates :color,
       presence: { message: "Please describe the cat's color" }
 
-   validates :location,
+  validates :location,
       presence: { message: "Where was this cat seen?" }
 
-   validates :last_seen,
+  validates :last_seen,
       presence: { message: "When was this cat last seen?" }
 
-   validates :temperament,
+  validates :temperament,
       inclusion: {
       in: %w[Friendly Shy Playful Hungry Scared Skittish Mysterious Grumpy Cuddly],
       message: "%{value} is not a valid temperament. Choose from: Friendly, Shy, Playful, Hungry, Scared, Skittish, Mysterious, Grumpy, Cuddly"
@@ -21,54 +21,54 @@ class Cat < ApplicationRecord
       allow_blank: true
 
 
-   validates :pattern,
+  validates :pattern,
       length: { maximum: 50, too_long: "Pattern description is too long (max 50 characters)" },
       allow_blank: true
 
-   validates :notes,
+  validates :notes,
       length: { maximum: 500, too_long: "Notes are too long (max 500 characters)" },
       allow_blank: true
 
-   validates :favorite_food,
+  validates :favorite_food,
       length: { minimum: 2, too_short: "Food must be at least 2 characters" }
 
-   validates :times_spotted,
+  validates :times_spotted,
       numericality: {
-         only_integer: true,
-         greater_than_or_equal_to: 0,
-         message: "must be non-negative number"
-      }
+        only_integer: true,
+        greater_than_or_equal_to: 0,
+        message: "must be non-negative number"
+      }, allow_blank: true
 
-   def spotted!
+  def spotted!
       update(times_spotted: times_spotted + 1)
       update(last_seen: Time.now)
-   end
+  end
 
-   def reset_spots
+  def reset_spots
       update(times_spotted: 0)
-   end
+  end
 
-   def repair_feeding_data!
+  def repair_feeding_data!
       # If data looks broken, fix it
       if times_fed_today > 3 || (last_fed_at.nil? && times_fed_today > 0)
       update(
-         times_fed_today: 0,
-         last_fed_at: nil
+        times_fed_today: 0,
+        last_fed_at: nil
       )
       puts "Repaired #{name}'s feeding data"
       end
-   end
+  end
 
-   def needs_feeding?
+  def needs_feeding?
       last_fed_at.nil? || last_fed_at < 6.hours.ago
-   end
+  end
 
-   def fed_recently?
+  def fed_recently?
       return false if last_fed_at.nil?
       last_fed_at > 2.hours.ago
-   end
+  end
 
-   def time_since_last_fed
+  def time_since_last_fed
       return "Never fed" if last_fed_at.nil?
 
       seconds = Time.now - last_fed_at
@@ -86,47 +86,47 @@ class Cat < ApplicationRecord
             days = (seconds / 86400).to_i
             "#{days} #{'day'.pluralize(days)} ago"
       end
-   end
+  end
 
-   def reset_daily_count_if_needed
+  def reset_daily_count_if_needed
       # Always reset if times_fed_today is 3 and it's a new day
       if times_fed_today >= 3 && (last_fed_at.nil? || last_fed_at < 24.hours.ago)
-         update(times_fed_at: 0)
-         true
+        update(times_fed_today: 0)
+        true
       elsif last_fed_at.nil? || last_fed_at < 24.hours.ago
-         update(times_fed_today: 0)
-         true
+        update(times_fed_today: 0)
+        true
       else
-         false
+        false
       end
-   end
+  end
 
-   def can_be_fed_today?
+  def can_be_fed_today?
       reset_daily_count_if_needed
       times_fed_today < 3
-   end
+  end
 
-   def daily_feeds_remaining
+  def daily_feeds_remaining
       reset_daily_count_if_needed
       3 - times_fed_today
-   end
+  end
 
-   def feed!
+  def feed!
       # ALWAYS reset if needed before checking
       reset_daily_count_if_needed
 
       if times_fed_today >= 3
-         errors.add(:base, "#{name} has already been fed 3 times today!")
-         return false
+        errors.add(:base, "#{name} has already been fed 3 times today!")
+        return false
       end
 
       update(
       last_fed_at: Time.now,
       times_fed_today: times_fed_today + 1
       )
-   end
+  end
 
-   def reset_daily_count
+  def reset_daily_count
       update(times_fed_today: 0)
-   end
+  end
 end
