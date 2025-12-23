af23f781645cb6d002336d68f49a287e7bb070e8
CorbinBlackard <59624967+CorbinBlackard@users.noreply.github.com>
2025-12-22 20:19:42 -0500

    most spotted cat on index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,13 @@
 class HomeController < ApplicationController
-  def index
-    @top_cats = Cat.order(times_spotted: :desc).limit(3)
-  end
+    def index
+      # Eager load photos to prevent N+1 queries
+      @top_cats = Cat.with_attached_photos.order(times_spotted: :desc).limit(3)
+
+      # Get today's most spotted cat (seen within 24 hours)
+      @king_block = Cat.where("last_seen >= ?", 24.hours.ago)
+                      .order(times_spotted: :desc)
+                      .first
+    end
 
   def guidelines
   end
