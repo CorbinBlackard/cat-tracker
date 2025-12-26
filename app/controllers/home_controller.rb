805104713ba895f458f61af2a4450de692fe6d72
CorbinBlackard <59624967+CorbinBlackard@users.noreply.github.com>
2025-12-26 08:37:44 -0500

    added the date to display below most spotted cats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,16 @@
 class HomeController < ApplicationController
-    @homepage = true
-    def index
-      # Eager load photos to prevent N+1 queries
-      @top_cats = Cat.with_attached_photos.order(times_spotted: :desc).limit(3)
+  def index
+    # Eager load photos
+    @top_cats = Cat.with_attached_photos
+                  .where("last_seen >= ?", 24.hours.ago)
+                  .order(times_spotted: :desc)
+                  .limit(3)
 
-      # Get today's most spotted cat (seen within 24 hours)
-      @king_block = Cat.where("last_seen >= ?", 24.hours.ago)
-                      .order(times_spotted: :desc)
-                      .first
-    end
+    # Today's most spotted cat
+    @king_block = Cat.where("last_seen >= ?", 24.hours.ago)
+                    .order(times_spotted: :desc)
+                    .first
+  end
 
   def guidelines
   end
